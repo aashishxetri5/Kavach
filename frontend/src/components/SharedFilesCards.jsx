889e0e4af588130cc19e892aec5ee81cf4509a67
nayanbanskota <nayanbanskota1@gmail.com>
2024-10-25 13:39:17 +0545

    New Files Added

---
+++ frontend/src/components/SharedFilesCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SharedFilesCards = () => {
+  return (
+    <div>
+      <h1>This cards contains the files that has been shared by the person 
+        with their respective name and id also mentioning the date associated with it
+      </h1>
+    </div>
+  )
+}
+
+export default SharedFilesCards
