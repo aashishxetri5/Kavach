8e47bdea5bf54021c35dc3a90f182a3c6f03f366
nayanbanskota <nayanbanskota1@gmail.com>
2024-10-25 11:28:56 +0545

    New Files Added

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BackButton = () => {
+  return (
+    <div>
+      <h1>This allows user to navigate back to original directory
+        from the current folder.
+      </h1>
+    </div>
+  )
+}
+
+export default BackButton
---
+++ frontend/src/components/InsideOfFolders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InsideOfFolders = () => {
+  return (
+    <div>
+      <h1>This allows user to navigate inside of a folder which
+        might contain files with some filters like File type and Date.
+      </h1>
+    </div>
+  )
+}
+
+export default InsideOfFolders
