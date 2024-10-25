b9b323e0663a11a1875792c2e5ba8e5f0d9f41a9
nayanbanskota <nayanbanskota1@gmail.com>
2024-10-25 15:12:11 +0545

    A new file is added.

---
+++ frontend/src/components/RenameFolders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RenameFolders = () => {
+  return (
+    <div>
+      <h1>Allow user to rename folders name</h1>
+    </div>
+  )
+}
+
+export default RenameFolders
