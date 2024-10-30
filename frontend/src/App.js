f5c3ddecf2b41c74350051cff73cf45e52f1d7e7
Aashish Katwal <aashiskutwal5@gmail.com>
2024-10-31 03:40:19 +0545

    Integrated components and their dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/output.css";
+
+import DashboardPage from "./pages/DashboardPage";
+import ErrorBoundary from "./utils/ErrorBoundary.utils";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <LoginPage />
-    </div>
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<DashboardPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './styles/output.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./styles/Homepage.css";
+import "./styles/output.css";
+import "./App.css";
 
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
