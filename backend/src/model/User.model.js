9ef1b5c43e04bc5e44c78371e656e5e8d936120c
Shradha Khatiwada <82402328+shradhakhatiwada@users.noreply.github.com>
2024-11-01 20:26:57 +0545

    Login Backend implemented using jwt

--- backend/src/model/User.model.js
+++ backend/src/model/User.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  fullname: {
     type: String,
     required: true,
   },
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  passwordHash: {
+  password: {
     type: String,
     required: true,
   },
