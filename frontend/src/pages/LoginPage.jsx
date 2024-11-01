744911964a5d29e0cc32c47ec7df207775a123ec
Shradha Khatiwada <82402328+shradhakhatiwada@users.noreply.github.com>
2024-11-01 18:58:51 +0545

    Login feature implemented.
    on submitting credentials it is sent to backend then response is received. jwt token is saved in localstorage.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,119 +1,158 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import Spinner from "../components/Spinner";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null); // State for error messages
+  const [success, setSuccess] = useState(null); // State for success messages
+  const [loading, setLoading] = useState(true); // State for loading
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      window.location.href = "/";
+    } else {
+      const timeoutId = setTimeout(() => {
+        setLoading(false); // Set loading to false after the delay
+      }, 2000); // 2 seconds delay for testing
+
+      return () => clearTimeout(timeoutId);
+
+      // setLoading(false);
+    }
+  }, [navigate]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setSuccess(null);
+
+    try {
+      const response = await axios.post(`/api/auth/login`, {
+        email,
+        password,
+      });
+
+      const { success, message, token } = response.data.result;
+
+      if (success) {
+        localStorage.setItem("token", token);
+        setSuccess(message);
+        navigate("/");
+      } else {
+        setError(message);
+      }
+
+      console.log("Login Response:", response.data.result);
+    } catch (error) {
+      console.error("Error in handleFormSubmit:", error);
+      setError("Something went wrong!! Try again.");
+    }
+  };
+
+  if (loading) return <Spinner />;
+
   return (
-    <div className="w-full max-w-md px-12 py-8 bg-white/[.95] rounded-lg shadow-lg">
-      <section>
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl">
-          Sign In
-        </h1>
-        <br />
-        <form action="login" method="post" className="lato">
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-1 text-sm font-semibold text-gray-900"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-              placeholder="john@example.com"
-              required
-            />
-            <span
-              className="block mb-1 text-sm text-red-700"
-              id="email-error-msg"
-            ></span>
-          </div>
-          <div className="mt-4">
-            <label
-              htmlFor="password"
-              className="block mb-1 text-sm font-bold text-gray-900"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              className="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 pr-10"
-              required
-            />
-            <span
-              className="block mb-3 text-sm text-red-700"
-              id="pwd-error-msg"
-            ></span>
-            <span
-              id="togglePassword"
-              className="password-toggle-icon absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer text-gray-700 dark:text-gray-300"
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-full max-w-md px-12 py-8 bg-white/[.95] rounded-lg shadow-lg">
+        <section>
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl">
+            Sign In
+          </h1>
+          <br />
+          <form onSubmit={handleFormSubmit} className="lato">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-1 text-sm font-semibold text-gray-900"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                className="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                placeholder="john@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <label
+                htmlFor="password"
+                className="block mb-1 text-sm font-bold text-gray-900"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                placeholder="••••••••••••"
+                className="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 pr-10"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="">
+              <span
+                className="block mb-1 text-sm text-red-700 text-center mt-2"
+                id="error-msg"
+              >
+                {error && <p style={{ color: "red" }}>{error}</p>}
+              </span>
+            </div>
+
+            <div className="flex items-center justify-end">
+              <div className="forgotpwd">
+                <Link
+                  to="#"
+                  className="text-sm font-light text-gray-700 hover:underline"
+                >
+                  Forgot Password?
+                </Link>
+              </div>
+            </div>
+
+            <div className="mt-4 flex justify-center">
+              <button
+                type="submit"
+                className="bg-primary w-2/6 text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 transition ease-in-out hover:scale-110"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <div className="mt-4 flex justify-center">
+            <Link
+              to="/"
+              className="text-gray-800 font-medium rounded-lg text-sm px-4 py-2 flex justify-center items-center underline hover:bg-gray-200/[.5] transition ease-in-out hover:scale-110"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 23 23"
-                strokeWidth="2"
-                stroke="#c2c2c2"
-                width="15"
-                height="15"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
+                  fill="black"
+                  d="m12 5.561l-7 5.6V19h5v-4h4v4h5v-7.358a1 1 0 0 0-.375-.781zM12 3l7.874 6.3A3 3 0 0 1 21 11.641V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7.839A2 2 0 0 1 3.75 9.6z"
+                ></path>
               </svg>
-            </span>
+              Back to Home
+            </Link>
           </div>
-          <div className="flex items-center justify-end">
-            <div className="forgotpwd">
-              <a
-                href="#"
-                className="text-sm font-light text-gray-700 hover:underline"
-              >
-                Forgot Password?
-              </a>
-            </div>
-          </div>
-          <div className="mt-4 flex justify-center">
-            <button
-              type="submit"
-              className="bg-primary w-2/6 text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 transition ease-in-out hover:scale-110"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-        <div className="mt-4 flex justify-center">
-          <a
-            href="/"
-            className="text-gray-800 font-medium rounded-lg text-sm px-4 py-2 flex justify-center items-center underline hover:bg-gray-200/[.5] transition ease-in-out hover:scale-110"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="black"
-                d="m12 5.561l-7 5.6V19h5v-4h4v4h5v-7.358a1 1 0 0 0-.375-.781zM12 3l7.874 6.3A3 3 0 0 1 21 11.641V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7.839A2 2 0 0 1 3.75 9.6z"
-              ></path>
-            </svg>
-            Back to Home
-          </a>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
