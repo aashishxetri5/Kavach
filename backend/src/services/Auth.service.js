d3d586d489b21eddb27ad29e025b901a2809bcf0
Shradha Khatiwada <82402328+shradhakhatiwada@users.noreply.github.com>
2024-11-01 20:25:21 +0545

    Login Backend implemented using jwt

---
+++ backend/src/services/Auth.service.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const User = require("../model/User.model");
+
+const validateUserCredentials = async (email, password) => {
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return { success: false, message: "Invalid email!! User doesn't exist." };
+    }
+
+    // Hash password
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
+    // Compare password
+    if (user.password !== hashedPassword) {
+      return { success: false, message: "Invalid password" };
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return { success: true, token };
+  } catch (error) {
+    console.error("Error in validateUserCredentials:", error);
+    return { success: false, message: "Something went wrong!! Try again." };
+  }
+};
+
+module.exports = { validateUserCredentials };
