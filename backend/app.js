c2ec5f4b6fce2f6a39ac1024ac024be5f0b2da84
Shradha Khatiwada <82402328+shradhakhatiwada@users.noreply.github.com>
2024-11-01 20:27:30 +0545

    Login Backend implemented using jwt

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,65 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const session = require("express-session");
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo");
+const fileUpload = require("express-fileupload");
+require("dotenv").config(); // Load environment variables from .env file
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const { connectToDatabase } = require("./database");
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const secret = process.env.SESSION_SECRET;
+const port = process.env.PORT;
+const connectionString = process.env.CONNECTION_STRING;
+
+// Initialize database connection
+(async function initializeApp() {
+  try {
+    await connectToDatabase(); // Connect to MongoDB
+
+    // Start the server after the connection is established
+    app.listen(port, () => {
+      console.log(`Server is running at port ${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to initialize the application:", err);
+    process.exit(1); // Exit process if initialization fails
+  }
+})();
+
+// Session middleware
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: connectionString,
+    }),
+    cookie: { maxAge: 120 * 60 * 1000 }, // 2 hours
+  })
+);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware for flash messages
+app.use(flash());
+
+// Middleware to handle file uploads
+app.use(fileUpload());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // enable set cookie
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Routes
+const routes = require("./src/routes");
+app.use("/", routes);
