45a41e869c520257f936e009adf6eb5b092a3ddd
Aashish Katwal <aashiskutwal5@gmail.com>
2024-11-04 13:39:40 +0545

    jwt token validation for files op

---
+++ backend/src/routes/file.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { authenticateToken } = require("../middleware/TokenValidation");
+
+const { uploadFile } = require("../controllers/File.controller");
+
+router.get("/home", authenticateToken, async (req, res) => {
+  res.send({ mesasge: "Hello World" });
+});
+
+router.post("/upload", authenticateToken, uploadFile);
+
+module.exports = router;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRouter = require("./auth");
+const fileRouter = require("./file");
 
 router.use("/api/auth", authRouter);
+router.use("/api/file", fileRouter);
 
 module.exports = router;
