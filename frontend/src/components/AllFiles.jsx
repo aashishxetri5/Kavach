66ed8391e3dc297f1b4e6585b09af8b05abacee2
Aashish Katwal <aashiskutwal5@gmail.com>
2024-11-04 21:38:38 +0545

    FE for all files fetch

--- frontend/src/components/AllFiles.jsx
+++ frontend/src/components/AllFiles.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { FileCards } from "./FileCards";
 
+import Spinners from "../components/Spinner";
+
 const AllFiles = () => {
+  const [files, setFiles] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  useEffect(() => {
+    const loadFiles = async () => {
+      try {
+        const instance = axios.create({
+          baseURL: "http://localhost:3000",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await instance.get("/api/file/all");
+        setFiles(data.data.files);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadFiles();
+  }, []);
+
+  if (loading) return <Spinners />;
+
   return (
     <div className="">
       <div className="title">
@@ -9,27 +39,9 @@ const AllFiles = () => {
       </div>
       <div className="fileList">
         <div className="flex flex-wrap items-center gap-4 w-full">
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
-          <FileCards />
+          {files.map((file) => (
+            <FileCards file={file} />
+          ))}
         </div>
       </div>
     </div>
