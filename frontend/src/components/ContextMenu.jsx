bfbd86e0a80889d5e6593ea38d3b45386debf832
Aashish Katwal <aashiskutwal5@gmail.com>
2024-12-07 19:55:27 +0545

    added share option and code refactor

--- frontend/src/components/ContextMenu.jsx
+++ frontend/src/components/ContextMenu.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import FileSharingModal from "./FileSharingModal";
 
 const ContextMenu = ({ file }) => {
+  const [currentfile, setCurrentFile] = useState(file);
+
   const handleDownload = async () => {
+    console.log("Download", file._id);
     const instance = axios.create({
       baseURL: "http://localhost:3000",
       headers: {
@@ -10,10 +14,15 @@ const ContextMenu = ({ file }) => {
         "Content-Type": "application/json",
       },
     });
-
-    const response = await instance.get(`/api/file/download`, {
+    let url;
+    if (file.file.filename.includes(".aes")) {
+      url = `/api/file/download`;
+    } else {
+      url = `/api/file/normaldownload`;
+    }
+    const response = await instance.get(url, {
       params: {
-        fileId: file.file._id, // Pass fileId as a query parameter
+        fileId: file._id, // Pass fileId as a query parameter
       },
       responseType: "blob",
     });
@@ -21,9 +30,17 @@ const ContextMenu = ({ file }) => {
     const blob = await response.data;
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = file.file.filename.replace(".aes", ""); // Set the default download file name
+    if (file.file.filename.includes(".aes")) {
+      link.download = file.file.filename.replace(".aes", ""); // Set the default download file name
+    } else {
+      link.download = file.file.filename;
+    }
     link.click();
   };
+
+  useEffect(() => {
+    setCurrentFile(file);
+  }, [file]);
 
   return (
     <div className="d-flex justify-content-between align-items-center ms-auto">
@@ -44,12 +61,12 @@ const ContextMenu = ({ file }) => {
           />
         </svg>
         <ul className="dropdown-menu cursor-pointer user-select-none py-2">
-          <li className="contextMenuItem">
-            <span className="dropdown-item items-center gap-2 py-3">
+          <li className="contextMenuItem" onClick={handleDownload}>
+            <span className="dropdown-item items-center gap-2 py-2.5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width={20}
+                height={20}
                 viewBox="0 0 512 512"
               >
                 <path
@@ -58,15 +75,38 @@ const ContextMenu = ({ file }) => {
                   d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v242.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64h384c35.3 0 64-28.7 64-64v-32c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352zm368 56a24 24 0 1 1 0 48a24 24 0 1 1 0-48"
                 />
               </svg>
-              <span onClick={handleDownload}>Download</span>
+              <span>Download</span>
             </span>
           </li>
-          <li className="contextMenuItem">
-            <span className="dropdown-item !flex items-center py-2">
+
+          <li
+            className="contextMenuItem"
+            data-bs-toggle="modal"
+            data-bs-target="#staticShareModal"
+          >
+            <span className="dropdown-item items-center gap-2 py-2.5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="black"
+                  className="contextMenuIcon"
+                  d="m13.576 17.271l-5.11-2.787a3.5 3.5 0 1 1 0-4.968l5.11-2.787a3.5 3.5 0 1 1 .958 1.755l-5.11 2.787a3.5 3.5 0 0 1 0 1.457l5.11 2.788a3.5 3.5 0 1 1-.958 1.755"
+                ></path>
+              </svg>
+              <span>Share</span>
+            </span>
+          </li>
+
+          <li className="contextMenuItem">
+            <span className="dropdown-item items-center gap-2 py-2.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={20}
+                height={20}
                 viewBox="0 0 448 512"
               >
                 <path
@@ -75,10 +115,12 @@ const ContextMenu = ({ file }) => {
                   d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0h120.4c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64s14.3-32 32-32h96zM32 128h384v320c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64zm96 64c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16m96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16m96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16"
                 />
               </svg>
-              <i className="fas fa-trash mx-2"></i> Delete
+              <span>Delete</span>
             </span>
           </li>
         </ul>
+
+        <FileSharingModal file={currentfile} />
       </div>
     </div>
   );
